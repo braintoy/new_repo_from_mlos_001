1a45261b43747d5fa055be2bc8525fd2cecc45cb
padmapolashpaul <padmapolashpaul@Padmas-MacBook-Pro-2.local>
2021-12-13 00:17:26 -0700

    Initial commit from mlOS.

--- app.py
+++ app.py
@@ -1,6 +1,78 @@
 import os
 import sys
+
+
+
+#Push updated form app001
+#Change
+
+
+#test change auto feom close
+
+import json
+
+# print(p)
+os.system("ls  >>xxx.txt")
+
+os.system(" pwd  >>xxx.txt")
+
 _cdir=os.path.dirname(os.path.abspath(__file__))
 sys.path.append(_cdir)
 
-push form APP 2
+
+tmp={
+    "key1":"value1",
+    "key1":"value1",
+    "key1":"value1",
+    "key1":"value1",
+    "key1":"value1"
+}
+import time
+print("Test herre")
+
+with open("tst.json",'w') as outfile:
+    json.dump(tmp,outfile)
+    
+import time
+
+time.sleep(1)
+print("SEC - 1")
+time.sleep(5)
+print("SEC - 2")
+time.sleep(5)
+print("SEC - 3")
+time.sleep(2)
+print("SEC - 4")
+time.sleep(1)
+print("SEC - 5")
+time.sleep(1)
+print("SEC - 6")
+time.sleep(1)
+print("SEC - 7")
+time.sleep(1)
+print("SEC - 8")
+time.sleep(1)
+print("SEC - 9")
+time.sleep(1)
+print("SEC - 10")
+time.sleep(1)
+print("SEC - 11")
+time.sleep(1)
+print("SEC - 12")
+time.sleep(1)
+print("SEC - 13")
+time.sleep(1)
+print("SEC - 1")
+time.sleep(1)
+print("SEC - 1")
+
+# for i in range (10):
+#     print(i)
+#     time.sleep(1)
+
+
+# print(x)
+
+
+#push frorm app 001
+
